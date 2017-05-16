95642b0398be2cfcc0b525caf13b2b2861b8a032
Dakalo <dakalo.mau@live.com>
2017-05-16 16:11:57 +0200

    Initial commit

---
+++ app/src/main/java/mysecondapp/screenapp/ScreenApp.java
@@ -0,0 +1,28 @@
+package mysecondapp.screenapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScreenApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_app);
+     Thread myThread = new Thread() {
+         @Override
+         public void run() {
+             try {
+                 sleep(3000);
+                 Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                 startActivity(intent);
+                 finish();
+             } catch (InterruptedException e) {
+                 e.printStackTrace();
+             }
+         }
+     };
+        myThread.start();
+    }
+}
